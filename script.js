6df395b1f172d544b21593d09a004239b9c8e871
STEPHANIE793-stack <StephanieHR07@hotmail.com>
2024-11-27 17:22:08 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ particlesJS("particles-js", {
       "value": 400,
       "density": {
         "enable": true,
-        "value_area": 800
-      }
-    },
+        "value_area": 800 } },
+
+
     "color": {
-      "value": "#fff"
-    },
+      "value": "#fff" },
+
     "shape": {
       "type": "circle",
       "stroke": {
         "width": 0,
-        "color": "#000000"
-      },
+        "color": "#000000" },
+
       "polygon": {
-        "nb_sides": 5
-      },
-    },
+        "nb_sides": 5 } },
+
+
     "opacity": {
       "value": 1,
       "random": false,
@@ -27,19 +27,19 @@ particlesJS("particles-js", {
         "enable": false,
         "speed": 1,
         "opacity_min": 0.1,
-        "sync": false
-      }
-    },
+        "sync": false } },
+
+
     "size": {
       "value": 10,
       "random": true,
       "anim": {
-        "enable": false,
-      }
-    },
+        "enable": false } },
+
+
     "line_linked": {
-      "enable": false,
-    },
+      "enable": false },
+
     "move": {
       "enable": true,
       "speed": 6,
@@ -51,48 +51,47 @@ particlesJS("particles-js", {
       "attract": {
         "enable": false,
         "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
+        "rotateY": 1200 } } },
+
+
+
   "interactivity": {
     "detect_on": "canvas",
     "events": {
       "onhover": {
         "enable": true,
-        "mode": "repulse"
-      },
+        "mode": "repulse" },
+
       "onclick": {
         "enable": true,
-        "mode": "repulse"
-      },
-      "resize": true
-    },
+        "mode": "repulse" },
+
+      "resize": true },
+
     "modes": {
       "grab": {
         "distance": 400,
         "line_linked": {
-          "opacity": 0.5
-        }
-      },
+          "opacity": 0.5 } },
+
+
       "bubble": {
         "distance": 100,
         "size": 4,
         "duration": 0.3,
         "opacity": 1,
-        "speed": 3
-      },
+        "speed": 3 },
+
       "repulse": {
         "distance": 100,
-        "duration": 0.4
-      },
+        "duration": 0.4 },
+
       "push": {
-        "particles_nb": 4
-      },
+        "particles_nb": 4 },
+
       "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
+        "particles_nb": 2 } } },
+
+
+
+  "retina_detect": true });
